82c27b9abfe2f3950ed764890a478ef9aa581d71
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-10-01 22:43:11 +0200

    Get more data from Twitter

--- app/src/components/LoginButton.tsx
+++ app/src/components/LoginButton.tsx
@@ -1,11 +1,13 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 export default function Component() {
-  const { data: session } = useSession();
+  const { data: session } = useSession() as any;
   if (session) {
     return (
       <>
-        Signed in as {session.user.name} <br />
+        Signed in as {session.user.image.username ?? session.user.name}
+        <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
       </>
     );
   }
--- app/src/pages/api/auth/[...nextauth].ts
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
-export const authOptions = {
+import { NextAuthOptions } from "next-auth";
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -13,13 +14,35 @@ export const authOptions = {
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: "2.0", // opt-in to Twitter OAuth 2.0
+
       // authorization: {
       //   params: {
       //     redirect_uri: "https://nft-id.vercel.app/api/auth/callback/github",
       //   },
       // },
+      userinfo: {
+        url: "https://api.twitter.com/2/users/me",
+        params: {
+          "user.fields":
+            "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld",
+        },
+      },
+      profile({ data }) {
+        return {
+          id: data.id,
+          image: data,
+        };
+      },
     }),
   ],
   secret: process.env.SECRET, // https://github.com/nextauthjs/next-auth/issues/3245#issuecomment-974772884
+  // callbacks: {
+  //   async jwt({ token, ...rest }) {
+  //     return { ...token, rest };
+  //   },
+  //   async session({ session, ...rest }) {
+  //     return { ...session, rest };
+  //   },
+  // },
 };
 export default NextAuth(authOptions);
