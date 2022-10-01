adc1d46f3d6ec75b78173e3527d22141abaf6e4e
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-10-01 15:50:54 +0200

    Comment out the TwitterProvider

--- app/src/pages/api/auth/[...nextauth].js
+++ app/src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+// import TwitterProvider from "next-auth/providers/twitter";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -8,10 +9,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    }),
+    // TwitterProvider({
+    //   clientId: process.env.TWITTER_CLIENT_ID,
+    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    // }),
   ],
 };
 export default NextAuth(authOptions);
