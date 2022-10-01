78a79ed2f75b51f3731136b29a831d323191a625
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-10-01 15:01:18 +0200

    Enable logging in with GitHub

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,34 +1,40 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { FC } from 'react';
-import { ContextProvider } from '../contexts/ContextProvider';
-import { AppBar } from '../components/AppBar';
-import { ContentContainer } from '../components/ContentContainer';
-import { Footer } from '../components/Footer';
-import Notifications from '../components/Notification'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { FC } from "react";
+import { ContextProvider } from "../contexts/ContextProvider";
+import { AppBar } from "../components/AppBar";
+import { ContentContainer } from "../components/ContentContainer";
+import { Footer } from "../components/Footer";
+import Notifications from "../components/Notification";
+import { SessionProvider } from "next-auth/react";
 
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
 
-const App: FC<AppProps> = ({ Component, pageProps }) => {
-    return (
-        <>
-          <Head>
-            <title>Solana Scaffold Lite</title>
-          </Head>
+const App: FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <>
+      <Head>
+        <title>Solana Scaffold Lite</title>
+      </Head>
 
-          <ContextProvider>
-            <div className="flex flex-col h-screen">
-              <Notifications />
-              <AppBar/>
-              <ContentContainer>
-                <Component {...pageProps} />
-              </ContentContainer>
-              <Footer/>
-            </div>
-          </ContextProvider>
-        </>
-    );
+      <ContextProvider>
+        <div className="flex flex-col h-screen">
+          <Notifications />
+          <AppBar />
+          <ContentContainer>
+            <SessionProvider session={session}>
+              <Component {...pageProps} />
+            </SessionProvider>
+          </ContentContainer>
+          <Footer />
+        </div>
+      </ContextProvider>
+    </>
+  );
 };
 
 export default App;
